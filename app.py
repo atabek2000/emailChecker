77a24c0913da781919a9fc95eeed089888130c94
Atabek <nuriddin.atabek2000@gmail.com>
2022-05-15 00:22:52 +0600

    third commit added comments in app.py added authors in about.html

--- app.py
+++ app.py
@@ -13,23 +13,33 @@ def text():
     if request.method == "GET":
         return render_template('text.html')
     elif request.method == "POST":
+        # Қолданушы мәтінін алу
         text = request.form['text']
+        # кіші әріптерге өзгерту
         if request.form['case'] == 'lower':
             text = text.lower()
+        # үлкен әріптерге өзгерту
         elif request.form['case'] == 'upper':
             text = text.upper()
+        # сандарды өшіру
         if request.form['number'] == '1':
             text = re.sub(r'\d', '', text)
+        # пунктуацияларды өшіру
         if request.form['punct'] == '1':
             text = re.sub(r'[^A-zа-яА-ЯІҢҒҮҰҚіңғүұқөӨһҺ ]', '', text)
+        # табуляцияны өшіру
         if request.form['tab'] == '1':
             text = re.sub(r'\t', '', text)
-        if request.form['space'] == '1':
-            text = re.sub(r' ', '', text)
+        # жалғыз символдарды өшіру
         if request.form['symbol'] == '1':
             text = re.sub(r'(^|\s).($|\s)', ' ', text)
+        # пробелдерді өшіру
+        if request.form['space'] == '1':
+            text = re.sub(r' ', '', text)
+        # жол үзілімдерін өшіру
         if request.form['l_break'] == '1':
             text = re.sub(r'\n', '', text)
+        # қолданушыға қайтатын жауап
         answer = '<script>$("#user_text").val("'+text+'")</script>'
         return answer
 
